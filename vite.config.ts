02cab6344c6c44bac2b62917af9ed76111823563
Sanjeet Sidhu <sanjeetu3@gmail.com>
2022-10-04 21:01:21 -0700

    added licence

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // following added to get HMR to work, didnt work out of the box... weird. https://github.com/vitejs/vite/issues/1153#issuecomment-785467271
   server: {
     watch: {
       usePolling: true
